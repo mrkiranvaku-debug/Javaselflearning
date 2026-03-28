bb3f81a73a173df918fa6ed37f7d7028dd963ba1
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-28 23:39:50 +0530

    This is datatype for boolean values program

---
+++ src/PrimitiveDataTypesinjava/BooleanType.java
@@ -0,0 +1,9 @@
+package PrimitiveDataTypesinjava;
+
+public class BooleanType {
+    public static void main(String[] args){
+        boolean isproductavailable= true;
+        boolean ispersonavailable= false;
+        System.out.println( "isproductavailable=====>"+isproductavailable);
+    }
+}
