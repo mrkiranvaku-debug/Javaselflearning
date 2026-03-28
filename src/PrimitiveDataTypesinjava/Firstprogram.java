d984bd3e12fb9f046ffeacbee43181d7e92a3d15
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-28 23:22:22 +0530

    This is datatypes program

--- src/PrimitiveDataTypesinjava/Firstprogram.java
+++
@@ -1,7 +0,0 @@
-package PrimitiveDataTypesinjava;
-
-public class Firstprogram {
-    public static void main(String[] args){
-        System.out.println("Hello welcome to my First program");
-    }
-}
---
+++ src/PrimitiveDataTypesinjava/Wholenumber.java
@@ -0,0 +1,10 @@
+package PrimitiveDataTypesinjava;
+
+public class Wholenumber {
+    public static void main(String[] args) {
+        byte firstNumber = 1;
+        short secondNumber = 320;
+        int thridNumber = 1000;
+        long fourthNumber = 5555555;
+    }
+}
