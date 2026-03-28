ac5c5d3180ba44972cfed20733935e2c6abe3168
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-28 23:29:30 +0530

    This is datatype character values program

---
+++ src/PrimitiveDataTypesinjava/Chardatatype.java
@@ -0,0 +1,10 @@
+package PrimitiveDataTypesinjava;
+
+public class Chardatatype {
+    public static void main(String[]args){
+        char productcode='A';
+        char lastnamefirstletter='D';
+        String name="Kiran karthick";
+        String initalletter="A";
+    }
+}
