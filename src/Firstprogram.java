1e26cd577f0c9ed3cbb40f11f1c5f98549ae7a5d
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-28 20:14:48 +0530

    Initial second commit to Javatraningbegining repository

--- src/Firstprogram.java
+++
@@ -1,5 +0,0 @@
-public class Firstprogram {
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
-    }
-}
---
+++ src/PrimitiveDataTypesinjava/Firstprogram.java
@@ -0,0 +1,7 @@
+package PrimitiveDataTypesinjava;
+
+public class Firstprogram {
+    public static void main(String[] args){
+        System.out.println("Hello welcome to my First program");
+    }
+}
