f84fd9ed415e0ff56190b66ca867eb5c0bcbf952
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-28 23:59:58 +0530

    This is string datatype program

---
+++ src/Nonprimitivedatatypes/Stringdatatypes.java
@@ -0,0 +1,11 @@
+package Nonprimitivedatatypes;
+
+public class Stringdatatypes {
+    public static void main(String[] args){
+        String firstpersonfirstname="kiran";
+        String firstpersonlastname="karthick";
+        String secondpersonfirstname= new String("aaryan");
+        String secondpersonsecondname= new String("kishore");
+        System.out.println(secondpersonfirstname);
+        }
+}
