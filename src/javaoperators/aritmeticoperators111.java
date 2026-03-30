c8986c7c3fa69231b6302adc0859633359672c75
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-30 19:00:02 +0530

    This is  arithmetic operators111 program

---
+++ src/javaoperators/aritmeticoperators111.java
@@ -0,0 +1,15 @@
+package javaoperators;
+
+public class aritmeticoperators111 {
+
+        public static void main(String[] args){
+            int firstnumber=200;
+            int secondnumber=300;
+            int sum=firstnumber+secondnumber;
+            System.out.println("Addition is:"+sum);
+            System.out.println("Subtraction is:" +(firstnumber-secondnumber));
+            System.out.print("Multiplication is:"+(firstnumber*secondnumber));
+        }
+    }
+
+
