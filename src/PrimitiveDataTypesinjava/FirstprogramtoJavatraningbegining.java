4a916b2130ded7ef8c17b49eae90f6da4d2df931
Kiran Karthick S <mrkiranvaku@gmail.com>
2026-03-30 18:57:57 +0530

    This is  arithmetic operators program

---
+++ src/PrimitiveDataTypesinjava/FirstprogramtoJavatraningbegining.java
@@ -0,0 +1,4 @@
+package PrimitiveDataTypesinjava;
+
+public class FirstprogramtoJavatraningbegining {
+}
---
+++ src/javaoperators/Arithmeticoperators.java
@@ -0,0 +1,12 @@
+package javaoperators;
+
+public class Arithmeticoperators {
+    public static void main(String[] args){
+        int firstnumber=200;
+        int secondnumber=300;
+        int sum=firstnumber+secondnumber;
+        System.out.println("Addition is:"+sum);
+        System.out.println("Subtraction is:" +(firstnumber-secondnumber));
+        System.out.print("Multiplication is:"+(firstnumber*secondnumber));
+    }
+}
